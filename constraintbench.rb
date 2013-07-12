cb8d29059f866f766191a85c61662eb84fe4cfd2
Tim Felgentreff <timfelgentreff@gmail.com>
2013-07-12 13:56:14 +0200

    constraintbench update for comparing performance of different constraint forumlations

--- constraintbench.rb
+++ constraintbench.rb
@@ -103,20 +103,13 @@ end
 
 
 
+Iterations = 100_000
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+def imperative
 mouse = Mouse.new
 mercury = Mercury.new
 thermometer = Thermometer.new(200.0, 0.0)
@@ -124,11 +117,10 @@ grey = Rectangle.new("grey", mercury.top, mercury.bottom)
 white = Rectangle.new("white", thermometer.top, mercury.top)
 temperature = mercury.height
 display = Display.new
-iterations = 10000000
 
 
 start = Time.now
-(0...iterations).each do |i|
+(0...Iterations).each do |i|
   mouse.location_y = i
   old = mercury.top
   mercury.top = mouse.location_y
@@ -146,23 +138,71 @@ start = Time.now
   display.number = temperature
   [mouse, mercury, thermometer, grey, white, temperature, display]
 end
-puts "Duration (#{iterations} iterations): #{Time.now - start}"
+puts "Duration (#{Iterations} iterations): #{Time.now - start}"
 puts [mouse, mercury, thermometer, grey, white, temperature, display].map { |e| e.inspect }
+end
+
+
+
+
+
+def library
+mouse_location_y = Cassowary::Variable.new value: 10
+mercury_top = Cassowary::Variable.new value: 10
+mercury_bottom = Cassowary::Variable.new value: 0
+mercury_height = mercury_top - mercury_bottom
+thermometer_top = Cassowary::Variable.new value: 200
+thermometer_bottom = Cassowary::Variable.new value: 0
+grey_top = Cassowary::Variable.new value: mercury_top.value
+grey_bottom = Cassowary::Variable.new value: mercury_bottom.value
+white_top = Cassowary::Variable.new value: thermometer_top.value
+white_bottom = Cassowary::Variable.new value: mercury_top.value
+temperature = Cassowary::Variable.new value: mercury_height.value
+display_number = Cassowary::Variable.new value: 0
+
+solver = Cassowary::SimplexSolver.new
+solver.auto_solve = false
+
+solver.add_constraint temperature == mercury_height
+solver.add_constraint white_top == thermometer_top
+solver.add_constraint white_bottom == mercury_top
+solver.add_constraint grey_top == mercury_top
+solver.add_constraint grey_bottom == mercury_bottom
+solver.add_constraint display_number == temperature
+constraint = mercury_top == mouse_location_y
+constraint.strength = Cassowary.symbolic_strength(:strong)
+solver.add_constraint constraint
+solver.add_constraint mercury_top <= thermometer_top
+solver.add_constraint mercury_bottom == thermometer_bottom
+solver.add_constraint thermometer_bottom == 0
+solver.add_constraint thermometer_top == 200
+
+solver.add_edit_var(mouse_location_y, Cassowary.symbolic_strength(:strong))
+solver.solve
+solver.begin_edit
+
+start = Time.now
+(0...Iterations).each do |i|
+  solver.suggest_value(mouse_location_y, i)
+  solver.resolve
+end
+solver.end_edit
+puts "Duration (#{Iterations} iterations): #{Time.now - start}"
+puts [mouse_location_y.value,
+      "#{mercury_top.value}->#{mercury_bottom.value}",
+      "#{thermometer_top.value}->#{thermometer_bottom.value}",
+      "#{grey_top.value}->#{grey_bottom.value}",
+      "#{white_top.value}->#{white_bottom.value}",
+      temperature.value,
+      display_number.value].map { |e| e.inspect }
+end
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+def ocp
 mouse = Mouse.new
 mercury = Mercury.new
 thermometer = Thermometer.new(200, 0)
@@ -183,9 +223,41 @@ always { mercury.bottom == thermometer.bottom }
 always { thermometer.bottom == 0 }
 always { thermometer.top == 200 }
 start = Time.now
-(0...iterations).each do |i|
+(0...Iterations).each do |i|
   mouse.location_y = i
-  [mouse, mercury, thermometer, grey, white, temperature, display]
+#  [mouse, mercury, thermometer, grey, white, temperature, display]
 end
-puts "Duration (#{iterations} iterations): #{Time.now - start}"
+puts "Duration (#{Iterations} iterations): #{Time.now - start}"
 puts [mouse, mercury, thermometer, grey, white, temperature, display].map { |e| e.inspect }
+end
+
+
+def editvars
+mouse = Mouse.new
+mercury = Mercury.new
+thermometer = Thermometer.new(200, 0)
+grey = Rectangle.new("grey", mercury.top, mercury.bottom)
+white = Rectangle.new("white", thermometer.top, mercury.top)
+temperature = mercury.height
+display = Display.new
+
+always { temperature == mercury.height }
+always { white.top == thermometer.top }
+always { white.bottom == mercury.top }
+always { grey.top == mercury.top }
+always { grey.bottom == mercury.bottom }
+always { display.number == temperature }
+always(:strong) { mercury.top == mouse.location_y }
+always { mercury.top <= thermometer.top }
+always { mercury.bottom == thermometer.bottom }
+always { thermometer.bottom == 0 }
+always { thermometer.top == 200 }
+start = Time.now
+
+edit((0...Iterations).each) { mouse.location_y }
+puts "Duration (#{Iterations} iterations): #{Time.now - start}"
+puts [mouse, mercury, thermometer, grey, white, temperature, display].map { |e| e.inspect }
+end
+
+
+send(ARGV[0])
