45ff3e6422349112ec146909f040a13b8fadaa7b
Tim Felgentreff <timfelgentreff@gmail.com>
2014-04-05 13:01:21 +0200

    babelsberg-r now

--- play.sh
+++ play.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wd="$(dirname "$0")"
-export LD_LIBRARY_PATH=$wd/../rupypy/dependencies/z3/build
-xterm -e $wd/../rupypy/bin/topaz-z3 $wd/bmp_streamer.rb $wd/video 2>/dev/null >/dev/null & 
+export LD_LIBRARY_PATH=$wd/../babelsberg-r/dependencies/z3/build
+$wd/../babelsberg-r/bin/topaz-z3 $wd/bmp_streamer.rb $wd/video 2>/dev/null >/dev/null
